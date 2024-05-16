94e3081c7fa3d246c3fe22dc40542408e21e010b
marcus <marcus.asberg.98@gmail.com>
2024-05-16 16:00:03 +0200

    Another quicksave

--- grid.h
+++ grid.h
@@ -1,4 +1,8 @@
-#pragma once
+#ifndef GRID_H
+#define GRID_H
+
+#include <vector>
+#include <raylib.h>
 
 class Grid
 {
@@ -6,10 +10,16 @@ public:
     Grid();
     void init(void);
     void print(void);
+    void Draw();
     int grid[20][10];
 
 private:
     int numRows;
     int numCols;
     int cellSize;
+
+    std::vector<Color> get_cell_color();
+    std::vector<Color> colors;
 };
+
+#endif
