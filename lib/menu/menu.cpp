2a891cf7824267629796bf2e87293ea3d8834db7
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-06-18 09:43:09 +0200

    Menu cpp added, removed duplicate in cmakelists

---
+++ lib/menu/menu.cpp
@@ -0,0 +1,16 @@
+#include <raylib.h>
+#include "menu.h"
+
+#include <raygui.h>
+
+Menu::Menu()
+{
+}
+
+void Menu::init(void)
+{
+}
+
+void Menu::Draw()
+{
+}
---
+++ lib/menu/menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H
+#define MENU_H
+
+#define RAYGUI_IMPLEMENTATION
+#include <raylib.h>
+
+class Menu
+{
+public:
+    Menu();
+    void init(void);
+    void Draw();
+
+private:
+};
+
+#endif
